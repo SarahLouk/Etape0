605c5cc0b2126194324e50d0bc526e3f2259650d
Thomas <thomas.delared@laposte.net>
2016-10-08 00:00:23 +0200

    peronne

--- src/terrain.cpp
+++ src/terrain.cpp
@@ -4,6 +4,7 @@
 
 #include <stdlib.h>
 #include "terrain.h"
+#include <math.h>
 
 #define LONGUEUR 512
 #define LARGEUR 128
@@ -52,9 +53,11 @@ bool** creation_terrain() {
             terrain[i][k] = false;
         }
     }
-    cout << "FIN" << terrain[0][126] << endl;
+
     return terrain;
 }
+
+
 void afficher_matrice(bool **terrain) {
     for(int i = 0; i < LONGUEUR; i++) {
         for(int j = 0; j < LARGEUR; j++) {
@@ -63,6 +66,179 @@ void afficher_matrice(bool **terrain) {
         cout << endl;
     }
 }
+
+
+bool isFree(bool **terrain,int x, int y){
+    for(int i=x-(TAILLE_P-1);i<=x;i++){
+        for(int j=y-(TAILLE_P-1);j<=y;j++){
+            if(!terrain[i][j]) return false;
+        }
+    }
+    return true;
+}
+
+bool isNotAWall(int x, int y){
+    for(int i=x-(TAILLE_P-1);i<=x;i++){
+        for(int j=y-(TAILLE_P-1);j<=y;j++){
+            if( (i<x_mur1 && (j<y_mur1_haut || j>y_mur1_bas)) ||
+                (i>x_mur2 && i<x_mur2_fin && (j<y_mur2_haut || j>y_mur2_bas))) return false;
+        }
+    }
+
+    return true;
+}
+
+
+personne * init_personnes(bool **terrain, int p){
+    personne* tab_personnes= (personne *) malloc(sizeof(personne) * pow(2,p)+1);
+    for(int i=0;i<pow(2,p);i++){
+        int x=LONGUEUR + rand() % (LONGUEUR-y_mur + (TAILLE_P-1));
+        int y=rand() % (LARGEUR - (TAILLE_P-1));
+
+        if(isFree(terrain,x,y)){
+            tab_personnes[i]= personne(x,y);
+            for(int i=x-(TAILLE_P-1);i<=x;i++){
+                for(int j=y-(TAILLE_P-1);j<=y;j++){
+                    terrain[i][j] = false;
+                }
+            }
+        }
+
+        else i--;
+    }
+
+    return tab_personnes;
+}
+
+float azimuth(personne p) {
+    return (float) sqrt(pow(p.x, 2) + pow(p.y - (LARGEUR / 2), 2));
+}
+
+/*
+ * Retourne 0 si le meilleur mouvement est au Nord, 1 si au NO, 2 si à l'O, 3 si au Su, 4 si au S
+ */
+int meilleur_coup(personne p){
+    int min =LONGUEUR;
+    int c;
+    //Nord
+    if(p.y>0 && min> azimuth(personne(p.x,p.y-1))){
+        min = azimuth(personne(p.x,p.y-1));
+        c=0;
+    }
+    //Nord Ouest
+    if(p.x>0 && p.y>0 && min> azimuth(personne(p.x-1,p.y-1))){
+        min = azimuth(personne(p.x-1,p.y-1));
+        c=1;
+    }
+    //Ouest
+    if(p.x>0 && min> azimuth(personne(p.x-1,p.y))) {
+        min = azimuth(personne(p.x-1,p.y));
+        c=2;
+    }
+    //Sud Ouest
+    if(p.y<LARGEUR && p.x>0 && min> azimuth(personne(p.x-1,p.y+1))){
+        min = azimuth(personne(p.x-1,p.y+1));
+        c=3;
+    }
+    //Sud
+    if(p.y<LARGEUR && min> azimuth(personne(p.x,p.y+1))) {
+        min = azimuth(personne(p.x,p.y+1));
+        c=4;
+    }
+
+    return c;
+}
+
+
+/*
+ * Argument personne peut etre pas bon mieux vaudrait tableau personne avec index de la personne
+ * sinon modif sur personne seront perdu
+ * A mouins que retourne une personne
+ */
+personne actualise(bool **terrain, personne p,int dir){
+    switch (dir){
+        case 0:
+            p = personne(p.x,p.y-1);
+            for(int i=0;i<TAILLE_P;i++){
+                terrain[p.x-i][p.y] = false;
+                terrain[p.x-i][p.y+TAILLE_P] = true;
+            }
+            break;
+
+        case 1:
+            p = personne(p.x-1,p.y-1);
+            for(int i=0;i<TAILLE_P;i++){
+                terrain[p.x-i][p.y] = false;
+                terrain[p.x-(i-1)][p.y+TAILLE_P] = true;
+            }
+            for(int i=1;i<TAILLE_P;i++){
+                terrain[p.x-(TAILLE_P-1)][p.y+i] = false;
+                terrain[p.x+1][p.y+i] = true;
+            }
+            break;
+
+        case 2:
+            p = personne(p.x-1,p.y);
+            for(int i=0;i<TAILLE_P;i++){
+                terrain[p.x-(TAILLE_P-1)][p.y+i] = false;
+                terrain[p.x+1][p.y+i] = true;
+            }
+            break;
+
+        case 3:
+            p = personne(p.x-1,p.y+1);
+            for(int i=0;i<TAILLE_P;i++){
+                terrain[p.x-(i-1)][p.y+1] = false;
+                terrain[p.x-i][p.y+(TAILLE_P-1)] = true;
+            }
+            for(int i=0;i<TAILLE_P-1;i++){
+                terrain[p.x+1][p.y+i] = false;
+                terrain[p.x-(TAILLE_P-1)][p.y+i] = true;
+            }
+            break;
+
+        case 4:
+            p = personne(p.x,p.y+1);
+            for(int i=0;i<TAILLE_P;i++){
+                terrain[p.x-i][p.y+(TAILLE_P-1)] = false;
+                terrain[p.x-i][p.y-1] = true;
+            }
+            break;
+    }
+
+    return p;
+}
+
+
+void deplacement(bool **terrain, personne p){
+    int dir = meilleur_coup(p);
+    switch (dir){
+        case 0:
+            if(isFree(terrain,p.x,p.y-1)) actualise(terrain,p,0);
+            break;
+
+        case 1:
+            if(isFree(terrain,p.x-1,p.y-1)) actualise(terrain,p,1);
+            else if(!isNotAWall(p.x-1,p.y-1)) actualise(terrain,p,0);
+            break;
+
+        case 2:
+            if(isFree(terrain,p.x-1,p.y)) actualise(terrain,p,2);
+            break;
+
+        case 3:
+            if(isFree(terrain,p.x-1,p.y+1)) actualise(terrain,p,3);
+            else if(!isNotAWall(p.x-1,p.y+11)) actualise(terrain,p,4);
+            break;
+
+        case 4:
+            if(isFree(terrain,p.x,p.y+1)) actualise(terrain,p,4);
+            break;
+    }
+
+}
+
+
 int main() {
     bool ** terrain = creation_terrain();
     afficher_matrice(terrain);
--- src/terrain.h
+++ src/terrain.h
@@ -12,11 +12,18 @@ using namespace std;
 typedef struct {
     int x;
     int y;
-} Personne;
+} personne;
+
 
 
 bool** creation_terrain();
 void afficher_matrice(bool ** terrain);
-void init_personnes(bool** terrain ,int p);
+bool isFree(bool **terrain,int x, int y);
+bool isNotAWall(int x, int y);
+personne* init_personnes(bool** terrain ,int p);
+float azimuth(personne p);
+int meilleur_coup(personne p);
+void deplacement(bool **terrain, personne p);
+personne actualise(bool **terrain, personne p,int dir);
 
 #endif //ETAPE0_TERRAIN_H
