d600af2b42afa969589adc83cd5e3a4e43e3fe0b
Thomas <thomas.delared@laposte.net>
2016-10-08 22:22:02 +0200

    personne

--- src/terrain.cpp
+++ src/terrain.cpp
@@ -7,19 +7,22 @@
 #include <string.h>
 #include <math.h>
 #include <unistd.h>
-#include <bits/stl_vector.h>
+#include <vector>
 
 #include "terrain.h"
 
+//Information concernant le terrain
 #define LONGUEUR 512
 #define LARGEUR 128
 #define TAILLE_P 4
 
+//Information concernant le mur 1 à céer sur le terrain
 int x_mur1 = 16;
 int y_mur1_haut = 60;
 int y_mur1_bas = 68;
 int y_mur = 128;
 
+//Information concernant le mur 2 à céer sur le terrain
 int x_mur2 = 112;
 int x_mur2_fin = 128;
 int y_mur2_haut = 56;
@@ -29,6 +32,9 @@ int NB_EXEC = 5;
 int NB_PERSONNES = 1;
 int NB_THREADS = 0;
 bool mActivee = false;
+
+//Fin du scénario
+bool fin = false;
 
 int getopt(int argc, char * const argv[],const char *optstring);
 extern char *optarg;
@@ -58,6 +64,9 @@ void lancer_statistiques(double *executionsCPU, double *executionsUtil) {
     }
 }
 
+/*
+ * Création du terrain
+ */
 bool** creation_terrain() {
     // Creation de la matrice
     bool **terrain = (bool **) malloc((LONGUEUR) * sizeof(bool *));
@@ -95,7 +104,9 @@ bool** creation_terrain() {
     return terrain;
 }
 
-
+/*
+ * Affichage du terrain
+ */
 void afficher_matrice(bool **terrain) {
     for(int i = 0; i < LONGUEUR; i++) {
         for(int j = 0; j < LARGEUR; j++) {
@@ -105,7 +116,11 @@ void afficher_matrice(bool **terrain) {
     }
 }
 
-
+/*
+ * Une personne est représenté par son pixel en haut à gauche (x,y) sont ses coordonnées
+ * On regarde si on peut poser une personne à cet endroit
+ * (cad qu'il n'existe rien sur la place qu'occuperait une personne)
+ */
 bool isFree(bool **terrain,int x, int y){
     for(int i=x-(TAILLE_P-1);i<=x;i++){
         for(int j=y-(TAILLE_P-1);j<=y;j++){
@@ -115,6 +130,10 @@ bool isFree(bool **terrain,int x, int y){
     return true;
 }
 
+/*
+ * Une personne est représenté par son pixel en haut à gauche (x,y) sont ses coordonnées
+ * On regarde si il y a un mur sur la place qu'occuperait la personne si on decidait de la creer en x;y
+ */
 bool isNotAWall(int x, int y){
     for(int i=x-(TAILLE_P-1);i<=x;i++){
         for(int j=y-(TAILLE_P-1);j<=y;j++){
@@ -126,19 +145,26 @@ bool isNotAWall(int x, int y){
     return true;
 }
 
-
+/*
+ * On creer une foule de taille 2**p sur le terrain
+ */
 vector<Personne> init_personnes(bool **terrain, int p){
-
     vector<Personne> tab_personnes;
+    //Pour chaque personne à créer on va choisir aléatoirement un x et y qui se situeront à l'est des murs
+    //et où il est possible de créer une personne à moins que quelqu'un ne soit déja là
     for(int i=0;i<pow(2,p);i++){
         int x=LONGUEUR + rand() % (LONGUEUR-y_mur + (TAILLE_P-1));
         int y=rand() % (LARGEUR - (TAILLE_P-1));
 
+        //On regarde si la place est libre pour les x;y pris aléatoirement puis oncrée la personne sur le terrain
         if(isFree(terrain,x,y)){
+            //On crée la personne
             Personne p;
             p._x = x;
             p._y = y;
+            //On stock la personne
             tab_personnes.push_back(p);
+            //On declare la place de la personne comme occupé maintenant
             for(int i=x-(TAILLE_P-1);i<=x;i++){
                 for(int j=y-(TAILLE_P-1);j<=y;j++){
                     terrain[i][j] = false;
@@ -146,20 +172,25 @@ vector<Personne> init_personnes(bool **terrain, int p){
             }
         }
 
+        //Si la place pour les x,y pris est déja occupé alors on refait un tour de plus
         else i--;
     }
 
     return tab_personnes;
 }
 
-//.erase pour gerer arrivé
-
+/*
+ * Calcul l'azimuth du coin haut droit d'une personne
+ * Revient à calculer l'hypothénuse du triangle rectangle entre (0;LARGEUR/2), (x;y) et (x;y-LARGEUR/2)
+ * La ligne LARGEUR/2 est "la distance la plus courte" pour tout y pour arriver à l'arrivé
+ */
 float azimuth(int x,int y) {
     return (float) sqrt(pow(x, 2) + pow(y - (LARGEUR / 2), 2));
 }
 
 /*
- * Retourne 0 si le meilleur mouvement est au Nord, 1 si au NO, 2 si à l'O, 3 si au Su, 4 si au S
+ * Calcul la direction qui rend le chemin le plus court pour une personne
+ * Retourne 0 si le meilleur mouvement est au Nord, 1 si au Nord Ouest, 2 si à l'Ouest, 3 si au Sud, 4 si au Sud
  */
 int meilleur_coup(Personne p){
     int min =LONGUEUR;
@@ -195,9 +226,9 @@ int meilleur_coup(Personne p){
 
 
 /*
- * Argument personne peut etre pas bon mieux vaudrait tableau personne avec index de la personne
- * sinon modif sur personne seront perdu
- * A mouins que retourne une personne
+ * Deplace la personne sur le terrain
+ * Set a false les nouveaux pixels occupés et true les pixels libérés
+ * pour la direction : 0 represente le Nord, 1 le Nord Ouest, 2 l'Ouest, 3 le Sud Ouest et 4 le Sud
  */
 Personne actualise(bool **terrain, Personne p,int dir){
     switch (dir){
@@ -256,39 +287,71 @@ Personne actualise(bool **terrain, Personne p,int dir){
 }
 
 
-void deplacement(bool **terrain, Personne p){
-    int dir = meilleur_coup(p);
+/*
+ * Gère le déplacement et arrivé d'une personne
+ * Avance dans la meilleure direction possible sinon attend que la place se libère
+ * Pour les deplacements diagonaux, si jamais un mur l'empeche d'avancer alors il se deplace verticalement (meilleur coup possible)
+ * On considère qu'une personne est arrivé lorsqu'au moins 1 pixel à dépasser la colonne x=0
+ */
+void deplacement(bool **terrain, vector<Personne> tab_personnes, int i){
+    int dir = meilleur_coup(tab_personnes[i]);
     switch (dir){
         //N
         case 0:
-            if(isFree(terrain,p._x,p._y-1)) actualise(terrain,p,0);
-            //else if wait;
+            if(isFree(terrain,tab_personnes[i]._x,tab_personnes[i]._y-1)) actualise(terrain,tab_personnes[i],0);
             break;
 
             //NO
         case 1:
-            if(isFree(terrain,p._x-1,p._y-1)) actualise(terrain,p,1);
-            else if(!isNotAWall(p._x-1,p._y-1)) actualise(terrain,p,0);
+            if(isFree(terrain,tab_personnes[i]._x-1,tab_personnes[i]._y-1)) actualise(terrain,tab_personnes[i],1);
+            else if(!isNotAWall(tab_personnes[i]._x-1,tab_personnes[i]._y-1) && isFree(terrain,tab_personnes[i]._x,tab_personnes[i]._y-1)) actualise(terrain,tab_personnes[i],0);
             break;
 
             //O
         case 2:
-            if(isFree(terrain,p._x-1,p._y)) actualise(terrain,p,2);
+            if(isFree(terrain,tab_personnes[i]._x-1,tab_personnes[i]._y)) actualise(terrain,tab_personnes[i],2);
+            else if(tab_personnes[i]._x==3){
+                tab_personnes.erase(tab_personnes.begin()+(i-1));
+                free(terrain,tab_personnes[i]._x,tab_personnes[i]._y);
+            }
             break;
 
             //SO
         case 3:
-            if(isFree(terrain,p._x-1,p._y+1)) actualise(terrain,p,3);
-            else if(!isNotAWall(p._x-1,p._y+11)) actualise(terrain,p,4);
+            if(isFree(terrain,tab_personnes[i]._x-1,tab_personnes[i]._y+1)) actualise(terrain,tab_personnes[i],3);
+            else if(!isNotAWall(tab_personnes[i]._x-1,tab_personnes[i]._y+1) && isFree(terrain,tab_personnes[i]._x,tab_personnes[i]._y+1)) actualise(terrain,tab_personnes[i],4);
             break;
 
             //S
         case 4:
-            if(isFree(terrain,p._x,p._y+1)) actualise(terrain,p,4);
+            if(isFree(terrain,tab_personnes[i]._x,tab_personnes[i]._y+1)) actualise(terrain,tab_personnes[i],4);
             break;
     }
 
 }
+
+/*
+ * Libère la place occuppé par la personne qui vient de franchir la ligne d'arrivée
+ */
+void free(bool **terrain,int x, int y){
+    for(int i=x-(TAILLE_P-1);i<=x;i++){
+        for(int j=y-(TAILLE_P-1);j<=y;j++){
+            terrain[i][j]=true;
+        }
+    }
+}
+
+/*
+ * Check si tout le monde est arrivé ou non
+ */
+bool finScenario(vector<Personne> tab_personnes){
+    if(tab_personnes.size()==0) {
+        fin = true;
+        return true;
+    }
+    return false;
+}
+
 
 /**
     Lance les scenarios
@@ -304,14 +367,21 @@ void executer (int n_personnes, int n_thread) {
         // On doit creer la matrice ici
         bool **terrain = creation_terrain();
         // On procede a l'initialisation des personnes
-        init_personnes(terrain, n_personnes);
+        vector<Personne> tab_personnes= init_personnes(terrain, n_personnes);
         clock_t chronoCPU;
         time_t chronoUtil;
+        //Si m activé on lance le chrono
         if (mActivee) {
             chronoCPU = clock();
             chronoUtil = time(NULL);
         }
         // Ici on est censee lancer le deplacement donc l'etape 0
+        while(!finScenario(tab_personnes)){
+            for(int i=0;i<tab_personnes.size();i++){
+                deplacement(terrain,tab_personnes,i);
+            }
+        }
+
         if (mActivee) { // Si option m activee
             chronoCPU = clock() - chronoCPU; // On recupere le temps CPU ecoule pour cette execution
             tempsExecCPU[i] = (double) chronoCPU/ CLOCKS_PER_SEC; // On place ce temps dans le tableau des mesures de temps CPU
--- src/terrain.h
+++ src/terrain.h
@@ -22,8 +22,10 @@ bool isNotAWall(int x, int y);
 vector<Personne> init_personnes(bool** terrain ,int p);
 float azimuth(Personne p);
 int meilleur_coup(Personne p);
-void deplacement(bool **terrain, Personne p);
+void deplacement(bool **terrain,vector<Personne> tab_personnes,int index);
 Personne actualise(bool **terrain, Personne p,int dir);
+bool finScenario(vector<Personne> tab_personnes);
+void free(bool **terrain,int x, int y);
 
 double moyenne(double *tableValues, int nbExecutions);
 void lancer_statistiques(double *executionsCPU, double *executionsUtil);
